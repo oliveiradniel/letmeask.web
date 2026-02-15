3ca14f9aa602cd3aa7f1132bf126c84e3cfe6511
oliveiradniel <kadadniel@gmail.com>
2026-02-15 17:42:18 -0300

    feat(data): setup React Query for server state management
    configure QueryClient and provider
    enable caching and async state handling

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,27 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import { CreateRoom } from "./pages/create-room";
 import { RoomDetails } from "./pages/room-details";
 
 export function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<CreateRoom />} index />
-        <Route element={<RoomDetails />} path="/room" />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<RoomDetails />} path="/room" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
