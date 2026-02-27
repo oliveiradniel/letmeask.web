3f4796031677ef3630be53a5a7cc570aa642e95a
oliveiradniel <kadadniel@gmail.com>
2026-02-26 22:07:43 -0300

    feat(rooms): map useQuery result for clearer naming
    - return `rooms` instead of `data`
    - rename `isFetching` to `isFetchingRooms` for clarity

--- src/hooks/use-list-rooms.ts
+++ src/hooks/use-list-rooms.ts
@@ -5,8 +5,13 @@ import { makeRoomsService } from "@/core/factories/make-rooms-service";
 export function useListRooms() {
   const roomsService = makeRoomsService();
 
-  return useQuery({
+  const { data, isFetching } = useQuery({
     queryKey: ["rooms"],
     queryFn: () => roomsService.list(),
   });
+
+  return {
+    rooms: data ?? [],
+    isFetchingRooms: isFetching,
+  };
 }
