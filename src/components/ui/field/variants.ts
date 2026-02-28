65e6ff155df39893cece95a905b9b48ad57787a8
oliveiradniel <kadadniel@gmail.com>
2026-02-28 07:57:39 -0300

    feat(ui): add Field component
    - add Separator component
    - add Label component
    - separate fieldVariants into variants.ts for clarity and reuse
    - add FieldSet, FieldLegend, FieldGroup, FieldContent
    - add FieldLabel, FieldTitle, FieldDescription
    - add FieldSeparator and FieldError

---
+++ src/components/ui/field/variants.ts
@@ -0,0 +1,36 @@
+import { cva, type VariantProps } from "class-variance-authority";
+
+export type FieldLegendProps = React.ComponentProps<"legend"> & {
+  variant?: "legend" | "label";
+};
+
+export type FieldProps = React.ComponentProps<"fieldset"> &
+  VariantProps<typeof fieldVariants>;
+
+export type FieldErrorProps = React.ComponentProps<"div"> & {
+  errors?: Array<{ message?: string } | undefined>;
+};
+
+export const fieldVariants = cva(
+  "group/field flex w-full gap-3 data-[invalid=true]:text-destructive",
+  {
+    variants: {
+      orientation: {
+        vertical: ["flex-col [&>*]:w-full [&>.sr-only]:w-auto"],
+        horizontal: [
+          "flex-row items-center",
+          "[&>[data-slot=field-label]]:flex-auto",
+          "has-[>[data-slot=field-content]]:items-start has-[>[data-slot=field-content]]:[&>[role=checkbox],[role=radio]]:mt-px",
+        ],
+        responsive: [
+          "@md/field-group:flex-row flex-col @md/field-group:items-center @md/field-group:[&>*]:w-auto [&>*]:w-full [&>.sr-only]:w-auto",
+          "@md/field-group:[&>[data-slot=field-label]]:flex-auto",
+          "@md/field-group:has-[>[data-slot=field-content]]:items-start @md/field-group:has-[>[data-slot=field-content]]:[&>[role=checkbox],[role=radio]]:mt-px",
+        ],
+      },
+    },
+    defaultVariants: {
+      orientation: "vertical",
+    },
+  }
+);
