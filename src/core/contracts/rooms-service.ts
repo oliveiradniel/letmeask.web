ee00c28fcf23c39ebe89e2f32a694998c3cd2c36
oliveiradniel <kadadniel@gmail.com>
2026-03-01 07:56:45 -0300

    feat(service-layer): implement create room
    - add create room schema and type
    - add generic type for request body
    - add abstract create room method
    - implement create room in rooms servisse

--- src/core/contracts/rooms-service.ts
+++ src/core/contracts/rooms-service.ts
@@ -1,5 +1,7 @@
 import type { Room } from "@/entities/room";
+import type { CreateRoomData } from "@/schemas/create-room-schema";
 
 export abstract class IRoomsService {
   abstract list(): Promise<Room[]>;
+  abstract create(data: CreateRoomData): Promise<Room>;
 }
--- src/core/services/rooms-service.ts
+++ src/core/services/rooms-service.ts
@@ -1,4 +1,5 @@
 import type { Room } from "@/entities/room";
+import type { CreateRoomData } from "@/schemas/create-room-schema";
 
 import type { IHttpClient } from "../contracts/http-client";
 import type { IRoomsService } from "../contracts/rooms-service";
@@ -14,5 +15,14 @@ export class RoomsService implements IRoomsService {
     const { data } = await this.httpClient.get<Room[]>("/rooms");
 
     return data;
+  }
+
+  async create(data: CreateRoomData): Promise<Room> {
+    const { data: createdRoom } = await this.httpClient.post<
+      CreateRoomData,
+      Room
+    >("/rooms", data);
+
+    return createdRoom;
   }
 }
