6e68215c2503d907df0bbdf9c41d838ae70d14ec
oliveiradniel <kadadniel@gmail.com>
2026-03-01 09:10:19 -0300

    feat(rooms): add create room form and implement room creation
    - add create room form layout
    - setup react-hook-form with Zod schema validation
    - implement room creation on form submit
    - update TanStack Query cache with the created room

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,6 +1,15 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useQueryClient } from "@tanstack/react-query";
+
 import { ArrowRight } from "lucide-react";
+
+import { useForm } from "react-hook-form";
+
 import { Link } from "react-router";
+
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,18 +17,108 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Field, FieldError, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Spinner } from "@/components/ui/spinner";
+import { Textarea } from "@/components/ui/textarea";
+import type { Room } from "@/entities/room";
+import { useCreateRoom } from "@/hooks/use-create-room";
 import { useListRooms } from "@/hooks/use-list-rooms";
+import {
+  type CreateRoomData,
+  CreateRoomSchema,
+} from "@/schemas/create-room-schema";
 import { formatTimeToNow } from "@/utils/format-relative-date";
 
 export function CreateRoom() {
+  const queryClient = useQueryClient();
+
   const { rooms, isFetchingRooms } = useListRooms();
+  const { createRoom, isCreatingRoom } = useCreateRoom();
+
+  const form = useForm({
+    mode: "onChange",
+    resolver: zodResolver(CreateRoomSchema),
+  });
+
+  const formDisabled = !form.formState.isValid || isCreatingRoom;
+
+  const handleSubmit = form.handleSubmit(async (data: CreateRoomData) => {
+    try {
+      const createdRoom = await createRoom(data);
+
+      queryClient.setQueryData<Room[]>(["rooms"], (old) => [
+        createdRoom,
+        ...(old ?? []),
+      ]);
+
+      form.reset();
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  });
 
   return (
     <div className="min-h-screen px-4 py-8">
       <div className="mx-auto max-w-4xl">
-        <div>
-          <div />
+        <div className="grid grid-cols-2 items-start gap-2">
+          <Card>
+            <CardHeader>
+              <CardTitle>Criar sala</CardTitle>
+
+              <CardDescription>
+                Crie uma nova sala para começar a fazer perguntas e receber
+                repostas da IA!
+              </CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                <Field>
+                  <FieldLabel
+                    className="after:text-destructive after:content-['*']"
+                    htmlFor="name"
+                  >
+                    Nome da sala
+                  </FieldLabel>
+
+                  <Input
+                    aria-invalid={!!form.formState.errors.name}
+                    id="name"
+                    placeholder="Digite o nome da sala"
+                    {...form.register("name")}
+                  />
+
+                  <FieldError>{form.formState.errors.name?.message}</FieldError>
+                </Field>
+
+                <Field>
+                  <FieldLabel htmlFor="description">Descrição</FieldLabel>
+
+                  <Textarea
+                    aria-invalid={!!form.formState.errors.description}
+                    id="description"
+                    {...form.register("description")}
+                  />
+
+                  <FieldError>
+                    {form.formState.errors.description?.message}
+                  </FieldError>
+                </Field>
+
+                <Button disabled={formDisabled} type="submit">
+                  {isCreatingRoom ? (
+                    <span className="flex items-center gap-1">
+                      <Spinner /> Criando sala...
+                    </span>
+                  ) : (
+                    "Criar sala"
+                  )}
+                </Button>
+              </form>
+            </CardContent>
+          </Card>
 
           <Card>
             <CardHeader>
