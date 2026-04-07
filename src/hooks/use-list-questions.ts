36f1d5a875e11ecabd217adac4dce444b626be0f
Daniel Oliveira <kadadniel@gmail.com>
2026-04-07 11:49:19 -0300

    feat(questions): create hook to list questions

---
+++ src/hooks/use-list-questions.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { makeRoomsService } from "@/core/factories/make-rooms-service";
+
+import type { Question } from "@/entities/question";
+
+export const QUESTIONS_QUERY_KEY = (roomId: string) => {
+  return ["questions", roomId];
+};
+
+export type QuestionQueryData = Question & { _isGeneratingAnswer?: boolean };
+
+export function useListQuestions(roomId: string) {
+  const roomsService = makeRoomsService();
+
+  const { data, isFetching } = useQuery({
+    queryKey: QUESTIONS_QUERY_KEY(roomId),
+    queryFn: async () => {
+      const questions = await roomsService.listQuestions(roomId);
+
+      return questions as QuestionQueryData[];
+    },
+  });
+
+  return {
+    questions: data ?? [],
+    isFetchingQuestions: isFetching,
+  };
+}
